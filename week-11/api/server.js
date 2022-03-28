58a4288eeeb6fec588d03ebc97360262e7372806
Ankitgiri Gusai <ankitgusai21@gmail.com>
2022-03-28 13:51:59 -0400

    cors allowed

--- week-11/api/server.js
+++ week-11/api/server.js
@@ -12,6 +12,7 @@ const parseURLParams = (value) => {
 };
 
 const server = http.createServer(async (req, res) => {
+  
   const [basePath, paramsString] = req.url.split("?");
 
   if (basePath === "/api/products" && req.method === "GET") {
@@ -34,8 +35,26 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+
 const PORT = 8888;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = server;
+module.exports = allowCors(server);
